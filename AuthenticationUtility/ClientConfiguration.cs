bb889501779c039dd4f3ac72898c0bea2323bfb7
Gjorgjiche Lazarovski <gogoaic@gmail.com>
2019-12-23 12:06:23 +0100

    SampeCredentails

---
+++ AuthenticationUtility/ClientConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationUtility
+{
+    public partial class ClientConfiguration
+    {
+        public static ClientConfiguration Default { get { return ClientConfiguration.OneBox; } }
+
+        public static ClientConfiguration OneBox = new ClientConfiguration()
+        {
+            // You only need to populate this section if you are logging on via a native app. For Service to Service scenarios in which you e.g. use a service principal you don't need that.
+            UriString = "", // 
+            UserName = "",
+            // Insert the correct password here for the actual test.
+            Password = "",
+
+            // You can find that under AAD in the azure portal
+            ActiveDirectoryResource = "", // "https://yoururl"
+            ActiveDirectoryTenant = "",
+            ActiveDirectoryClientAppId = "",
+            // Insert here the application secret when authenticate with AAD by the application
+            ActiveDirectoryClientAppSecret = "",
+
+            // Change TLS version of HTTP request from the client here
+            // Ex: TLSVersion = "1.2"
+            // Leave it empty if want to use the default version
+            TLSVersion = "",
+        };
+
+        public string TLSVersion { get; set; }
+        public string UriString { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ActiveDirectoryResource { get; set; }
+        public String ActiveDirectoryTenant { get; set; }
+        public String ActiveDirectoryClientAppId { get; set; }
+        public string ActiveDirectoryClientAppSecret { get; set; }
+    }
+}
